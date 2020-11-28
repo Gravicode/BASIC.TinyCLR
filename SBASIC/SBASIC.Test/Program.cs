ef8a831103740977da78d2f71ba47198e1b0005c
Mif Masterz <mifmasterz@yahoo.com>
2020-11-28 10:33:41 +0700

    init

---
+++ SBASIC/SBASIC.Test/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using GHIElectronics.TinyCLR.Pins;
+using SBASIC.Library;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+namespace Basic.Test
+{
+    class Program
+    {
+       
+        static void Main()
+        {
+            //use UART5 on SITCore SC20260
+
+            //basic testing
+            var module = new SBasicEngine(SC20260.UartPort.Uart5);
+            module.Run();
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+
+    }
+}
